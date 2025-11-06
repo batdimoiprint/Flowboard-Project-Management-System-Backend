4489f0e638f1ff05999dbd5b44f09dc8c301b35e
batdimoiprint <johnkennyreyes21@gmail.com>
2025-11-07 06:02:43 +0800

    Update CORS policy to allow requests from the production frontend URL

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
         policy => policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("https://flowboard.azurewebsites.net")
             .AllowAnyHeader()
             .AllowAnyMethod()
     );
