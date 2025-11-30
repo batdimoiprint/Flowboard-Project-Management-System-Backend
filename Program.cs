f1f318bcaa734a762ec5d541b5d313bbda1bbf70
Kenny Reyes <johnkennyreyes21@gmail.com>
2025-11-30 15:01:57 +0000

    Enhance JSON serialization options for controllers to use camelCase naming

--- Program.cs
+++ Program.cs
@@ -2,12 +2,19 @@
 using Flowboard_Project_Management_System_Backend.Services;
 using Flowboard_Project_Management_System_Backend.Configurations;
 using DotNetEnv;
+using System.Text.Json;
 
 // Builder
 var builder = WebApplication.CreateBuilder(args);
 Env.Load();
 builder.Services.AddFrontendCors(builder.Environment);
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Use camelCase for JSON property names to match frontend expectations
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    });
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<MongoDbService>();
 builder.Services.AddJwtAuthentication();
